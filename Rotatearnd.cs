a76cea9060eef871c16337340c86f305c0f97597
Christopher Hayes <chrisdhjx@gmail.com>
2017-08-10 02:04:18 +1200

    Add files via upload

---
+++ Rotatearnd.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotatearnd : MonoBehaviour {
+
+    public Vector3 spinSpeed = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+        spinSpeed = new Vector3(Random.value, Random.value, Random.value);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Rotate(spinSpeed);
+        this.transform.RotateAround(Vector3.zero, Vector3.up, 1.0f);
+	}
+}
